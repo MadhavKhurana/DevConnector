8d3300e639ac7778ab7a26de613402890018b864
Madhav <madhavkhurana086@gmail.com>
2019-06-06 19:15:44 +0530

    Prepared for deploy

--- server.js
+++ server.js
@@ -1,45 +1,47 @@
-const express=require('express');
-const mongoose=require('mongoose')
-const users=require('./routes/api/users.js')
-const profile=require('./routes/api/profile.js')
-const posts=require('./routes/api/posts.js')
-const bodyParser=require('body-parser')
-const passport=require('passport')
-const path=require('path')
+const express = require("express");
+const mongoose = require("mongoose");
+const users = require("./routes/api/users.js");
+const profile = require("./routes/api/profile.js");
+const posts = require("./routes/api/posts.js");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
 
-const app=express()
+const app = express();
 
 //Body parser middleware
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //db config
-const db=require('./config/keys').mongoURI
+const db = require("./config/keys").mongoURI;
 
 //connect to mongoDB
-mongoose.connect(db).then(()=>console.log('MongoDb connected')).catch(err=>console.log(err))
+mongoose
+    .connect(db)
+    .then(() => console.log("MongoDb connected"))
+    .catch(err => console.log(err));
 
 //passport middleware
-app.use(passport.initialize())
+app.use(passport.initialize());
 
 //passport config
-require('./config/passport')(passport)
+require("./config/passport")(passport);
 
+//ap
 
-//ap 
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
-app.use('/api/users',users);
-app.use('/api/profile',profile);
-app.use('/api/posts',posts)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
 
-if(process.env.NODE_ENV==='production'){
-    app.use(express.static('client/build'))
-    
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html')
-    })
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
 }
 
-const port =process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.listen(port,()=>console.log(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
